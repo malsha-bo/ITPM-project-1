763dfa2ac8adf714b051e9922c1ab77a4b4bcf95
Adeesha <mendisadeesha@gmail.com>
2021-03-26 03:55:05 +0530

    ITPM Project

--- src/itpm_projectnb/Home/AddStudentGroupsController.java
+++ src/itpm_projectnb/Home/AddStudentGroupsController.java
@@ -7,14 +7,25 @@ package itpm_projectnb.Home;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
 
 /**
  * FXML Controller class
  *
  * @author Adeesha
  */
-public class AddStudentController implements Initializable {
+public class AddStudentGroupsController implements Initializable {
+
+    @FXML
+    private ComboBox<?> AcYrComb;
+    @FXML
+    private ComboBox<?> programComb;
+    @FXML
+    private ComboBox<?> groupNoComb;
+    @FXML
+    private ComboBox<?> subgroupNoComb;
 
     /**
      * Initializes the controller class.
